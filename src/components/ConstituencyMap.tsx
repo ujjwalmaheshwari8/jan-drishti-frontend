7c65eb1b232edf315f62591c2c7613acaabe3eeb
Ujjwal Maheshwari <ujjwal.maheshwari7@gmail.com>
2026-03-21 00:25:25 +0530

    Fix: Add Leaflet CDN and force map invalidateSize

--- src/components/ConstituencyMap.tsx
+++ src/components/ConstituencyMap.tsx
@@ -29,7 +29,7 @@ const ConstituencyMap = ({ constituency: constituencyProp, complaints: complaint
     mapInstance.current = map;
 
    // Replace the old L.tileLayer block with this one:
-L.tileLayer("https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png", {
+L.tileLayer("https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png", {
   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
   subdomains: 'abcd',
   maxZoom: 20
